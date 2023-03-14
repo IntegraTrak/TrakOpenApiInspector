245045cb820991b1d345f30caf190e5859486645
Greg Brewer <greg.brewer@trakstudios.com>
2023-03-14 02:29:13 -0400

    Add eslint, prettier, vitest, testing-library

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,32 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+/* eslint-disable import/no-extraneous-dependencies */
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/tests/setupTests.ts'],
+  },
   server: {
     host: true,
     port: 5150,
     watch: {
       usePolling: true,
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        import: resolve(__dirname, 'data-import.html'),
+      },
     },
   },
 });
