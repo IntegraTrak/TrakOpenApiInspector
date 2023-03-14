a6267dc29ce4f7cb9d9f8c4fdc603e3ac7014432
Greg Brewer <greg.brewer@trakstudios.com>
2023-03-14 02:29:53 -0400

    initial tests

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import App from '../App';
+
+describe('App', () => {
+  it('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Trak Open API Inspector/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
