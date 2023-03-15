6837c680f4718a369c8574258b7be7bea9dc7b4f
Greg Brewer <greg.brewer@trakstudios.com>
2023-03-15 12:15:58 -0400

    add initial tests

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import { describe, it } from 'vitest';
-import App from '../App';
+import { render, screen } from "@testing-library/react";
+import { describe, it } from "vitest";
+import App from "../App";
 
-describe('App', () => {
-  it('renders learn react link', () => {
+describe("App", () => {
+  it("renders learn react link", () => {
     render(<App />);
     const linkElement = screen.getByText(/Trak Open API Inspector/i);
     expect(linkElement).toBeInTheDocument();
